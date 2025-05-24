9ae467a02b29ce53cbd58396e41db68e7b2a7a53
Jonathan GAYVALLET <jonathan.mael.gayvallet@gmail.com>
2025-05-24 21:44:30 +0200

    push thinkpad conf

--- flake.nix
+++ flake.nix
@@ -1,10 +1,27 @@
 {
-  description = "A very basic flake";
+  description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+	{
+	  home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.jonathan = ./home.nix;
+        }
+      ];
+    };
   };
 }
